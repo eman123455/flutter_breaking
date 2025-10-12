d8c53db1c25ff19c9efc4d0b2613c2110599420f
Eman Kamel <kamele432@gmail.com>
2025-10-13 01:46:30 +0300

    add  charactter details route to app route file

--- lib/utils/app_router.dart
+++ lib/utils/app_router.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_breaking/constants/strings.dart';
+import 'package:flutter_breaking/presentation/screens/character_details.dart';
 import 'package:flutter_breaking/presentation/screens/characters_screen.dart';
 
 class AppRouter {
   static Route? generateapproute(RouteSettings setting) {
     switch (setting.name) {
-      case '/':
-        return MaterialPageRoute(builder: (_)=>CharactersScreen());
+      case characterScreen:
+        return MaterialPageRoute(builder: (_) => CharactersScreen());
+
+      case characterDetails:
+        return MaterialPageRoute(builder: (_) => CharacterDetails());
     }
   }
 }
