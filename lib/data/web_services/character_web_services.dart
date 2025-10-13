b05abf488c7a86f6eecfee575ea599c2a03ff12c
Eman Kamel <kamele432@gmail.com>
2025-10-13 04:00:05 +0300

    create character web services

---
+++ lib/data/web_services/character_web_services.dart
@@ -0,0 +1,29 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter_breaking/constants/strings.dart';
+import 'package:flutter_breaking/data/models/character_model/character_model.dart';
+
+class CharacterWebServices {
+  late Dio dio;
+
+  CharacterWebServices() {
+    BaseOptions baseOptions = BaseOptions(
+        baseUrl: baseUrl,
+        receiveDataWhenStatusError: true,
+        connectTimeout: const Duration(seconds: 20),
+        receiveTimeout: const Duration(seconds: 20));
+
+    dio = Dio(baseOptions);
+  }
+
+  Future<List<CharacterModel>> getAllCharacter() async {
+    try {
+      Response response = await dio.get('/character');
+      print(response.data.toString());
+      return response.data;
+    } catch (e) {
+      return [];
+    }
+  }
+}
