175ab9d340a27edd05a34ff1757827520cd75e71
Eman Kamel <kamele432@gmail.com>
2025-10-22 03:41:24 +0300

    imolement character screen

--- lib/presentation/screens/characters_screen.dart
+++ lib/presentation/screens/characters_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_breaking/bussiness_logic/cubit/character_cubit.dart';
+import 'package:flutter_breaking/constants/my_colors.dart';
+import 'package:flutter_breaking/presentation/widgets/characters_list.dart';
 
 class CharactersScreen extends StatelessWidget {
   const CharactersScreen({super.key});
@@ -6,7 +10,11 @@ class CharactersScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('data'),
-    );
+        backgroundColor: MyColors.myGrey,
+        appBar: AppBar(
+          backgroundColor: MyColors.myYellow,
+          title: const Text('Characters'),
+        ),
+        body: CharactersList());
   }
 }
